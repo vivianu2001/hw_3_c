ea3b87057342aa1919c357b8df42306bbca20ac9
vivianu2001 <118671563+vivianu2001@users.noreply.github.com>
2024-02-20 12:49:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,104 +13,52 @@ int main() {
     int numWords;
     
     while (1) {
-        printf("\nOptions:\n");
-        printf("1- Insert strings into the list.\n");
-        printf("2- Insert a string at a certain index.\n");
-        printf("3- Print the list.\n");
-        printf("4- Print the length of the list.\n");
-        printf("5- Print a string at a certain index.\n");
-        printf("6- Print the number of characters in the entire list.\n");
-        printf("7- Receive a string and print how many times it appears.\n");
-        printf("8- Receive a string and delete all occurrences from the list.\n");
-        printf("9- Receive an index and delete the corresponding member.\n");
-        printf("10- Reverse the list.\n");
-        printf("11- Delete the entire list.\n");
-        printf("12- Sort the list in lexicographical order.\n");
-        printf("13- Check whether the list is arranged according to lexicographic order.\n");
-        printf("0- Exit the program.\n");
-        printf("Choose an option: ");
-        scanf(" %d", &option);
+        scanf("%d", &option);
+        
         switch(option) {
-    case 1: // Insert strings into the list
-        char choice;
-        scanf(" %c", &choice);
-        if (choice != 'A') {
-            printf("Invalid choice.\n");
-            break;
-        }
-        printf("Enter number of words: ");
-        scanf("%d", &numWords);
-        printf("Enter strings separated by space:\n");
-        for (int i = 0; i < numWords; i++) {
-            scanf("%s", buffer);
-            StrList_insertLast(&list_sentinel, buffer);
-        }
-        break;
-
-
-            case 2: // Insert a string at a certain index
-                printf("Enter index: ");
-                scanf("%d", &index);
-                printf("Enter string: ");
-                scanf("%s", buffer);
-                StrList_insertAt(&list_sentinel, buffer, index);
-                break;
-            case 3: // Print the list
-                printf("List: ");
-                StrList_print(&list_sentinel);
-                break;
-            case 4: // Print the length of the list
-                printf("Length of the list: %zu\n", StrList_size(&list_sentinel));
-                break;
-            case 5: // Print a string at a certain index
-                printf("Enter index: ");
-                scanf("%d", &index);
-                printf("String at index %d: ", index);
-                StrList_printAt(&list_sentinel, index);
-                break;
-            case 6: // Print the number of characters in the entire list
-                printf("Number of characters in the list: %d\n", StrList_printLen(&list_sentinel));
-                break;
-            case 7: // Receive a string and print how many times it appears
-                printf("Enter string: ");
-                scanf("%s", buffer);
-                printf("String '%s' appears %d times in the list.\n", buffer, StrList_count(&list_sentinel, buffer));
-                break;
-            case 8: // Receive a string and delete all occurrences from the list
-                printf("Enter string to delete: ");
-                scanf("%s", buffer);
-                StrList_remove(&list_sentinel,buffer);
-                break;
-            case 9: // Receive an index and delete the corresponding member
-                printf("Enter index to delete: ");
-                scanf("%d", &index);
-                StrList_removeAt(&list_sentinel, index);
-                break;
-            case 10: // Reverse the list
-                StrList_reverse(&list_sentinel);
-                printf("List reversed.\n");
-                break;
-            case 11: // Delete the entire list
-                StrList_free(&list_sentinel);
-                printf("List deleted.\n");
-                list_sentinel.next=NULL;
-                break;
-            case 12: // Sort the list in lexicographical order
-                StrList_sort(&list_sentinel);
-                printf("List sorted.\n");
-                break;
-            case 13: // Check whether the list is arranged according to lexicographic order
-                if (StrList_isSorted(&list_sentinel)) {
-                    printf("List is arranged in lexicographic order.\n");
-                } else {
-                    printf("List is not arranged in lexicographic order.\n");
+            case 1: // Insert strings into the list
+                scanf("%d", &numWords);
+                for (int i = 0; i < numWords; i++) {
+                    scanf("%s", buffer);
+                    StrList_insertLast(&list_sentinel, buffer);
                 }
                 break;
+
+            case 3: // Print the list
+                StrList_print(&list_sentinel);
+                break;
+
+            case 4: // Print the length of the list
+                printf("%d\n", (int)StrList_size(&list_sentinel));
+                break;
+
+            case 13: // Check whether the list is arranged according to lexicographic order
+                if (StrList_isSorted(&list_sentinel)) {
+                    printf("true\n");
+                } else {
+                    printf("false\n");
+                }
+                break;
+
+            case 5: // Print a string at a certain index
+                scanf("%d", &index);
+                StrList_printAt(&list_sentinel, index);
+                break;
+
+            case 6: // Print the number of characters in the entire list
+                printf("%d\n", StrList_printLen(&list_sentinel));
+                break;
+
+            case 10: // Reverse the list
+                StrList_reverse(&list_sentinel);
+                break;
+
             case 0: // Exit the program
                 StrList_free(&list_sentinel);
                 return 0;
+
             default:
-                printf("Invalid option. Please choose a valid option.\n");
+                break;
         }
     }
 
